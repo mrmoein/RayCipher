5309040290ad12e93c57de0022c36d0b7333a532
Moein Aghamirzaei <moein.6556@gmail.com>
2025-12-30 22:50:49 +0330

    fix PWA

--- script.js
+++ script.js
@@ -245,5 +245,5 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("./sw.js", { scope: "./" });
+	navigator.serviceWorker.register("sw.js");
 }
